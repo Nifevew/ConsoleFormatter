868463185751cb648f68de260f6f6888c55d95c0
Max <max.yablokov02@yandex.ru>
2024-01-20 02:21:11 +0300

    upd main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include <sstream>
+
+#include "./include/TextFormatting.hpp"
+
 int main()
 {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << tf::TextColor::RED << tf::TextStyle::BOLD << "TEST_TEXT" << std::endl;
+  std::cout << tf::TextStyle::DEFAULT << tf::TextColor::DEFAULT;
+  std::cout << tf::setTextParams(tf::TextColor::GREEN, tf::TextStyle::UNDERLINE, tf::TextStyle::BOLD) << "TEST_TEXT" << std::endl;
+
   return 0;
 }
