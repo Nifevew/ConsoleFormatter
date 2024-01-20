be9f717d837cf1271d793ead5c8f745b5e0bd6f0
Max <max.yablokov02@yandex.ru>
2024-01-20 17:01:06 +0300

    upd main
    Signed-off-by: Max <max.yablokov02@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -7,8 +7,35 @@
 int main()
 {
   std::cout << tf::TextColor::RED << tf::TextStyle::BOLD << "TEST_TEXT" << std::endl;
-  std::cout << tf::TextStyle::DEFAULT << tf::TextColor::DEFAULT;
+  std::cout << tf::TextStyle::RESET << tf::TextColor::DEFAULT;
   std::cout << tf::setTextParams(tf::TextColor::GREEN, tf::TextStyle::UNDERLINE, tf::TextStyle::BOLD) << "TEST_TEXT" << std::endl;
+
+
+  std::cout << tf::TextStyle::BOLD << "BOLD_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::DIM << "DIM_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::ITALIC << "ITALIC_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::UNDERLINE << "UNDERLINE_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::BLINKING << "BLINKING_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::INVERSE << "INVERSE_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::HIDDEN << "HIDDEN_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::TextStyle::STRIKETHROUGH << "STRIKETHROUGH_TEXT\n";
+  std::cout << tf::TextStyle::RESET;
+
+  std::cout << tf::setTextParams(tf::TextStyle::ITALIC, tf::TextColor::GREEN, tf::BackgroundColor::BLUE) << "TEST_TEXT\n";
 
   return 0;
 }
