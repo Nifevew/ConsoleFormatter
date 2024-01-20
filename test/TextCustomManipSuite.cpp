6f22b5418475fae305d35bdf149abfe5ade1e1fe
Max <max.yablokov02@yandex.ru>
2024-01-20 14:40:13 +0300

    Тестирование манипулятора для цвета фона
    Signed-off-by: Max <max.yablokov02@yandex.ru>

--- test/TextCustomManipSuite.cpp
+++ test/TextCustomManipSuite.cpp
@@ -50,14 +50,26 @@ TEST(TextFormattingTextCustomManipSuite, TwoStyle)
   EXPECT_EQ(test_str, true_str);
 }
 
-TEST(TextFormattingTextCustomManipSuite, StyleBoldBright)
+TEST(TextFormattingTextCustomManipSuite, OneBackground)
 {
   std::stringstream ss;
 
-  ss << tf::setTextParams(tf::TextStyle::BOLD, tf::TextStyle::BRIGHT) << "TEST_TEXT";
+  ss << tf::setTextParams(tf::BackgroundColor::BLUE) << "TEST_TEXT";
   std::string test_str { ss.str() };
 
-  std::string true_str { "\x1b[1;1mTEST_TEXT" };
+  std::string true_str { "\x1b[44mTEST_TEXT" };
+
+  EXPECT_EQ(test_str, true_str);
+}
+
+TEST(TextFormattingTextCustomManipSuite, TwoBackground)
+{
+  std::stringstream ss;
+
+  ss << tf::setTextParams(tf::BackgroundColor::BLUE, tf::BackgroundColor::WHITE) << "TEST_TEXT";
+  std::string test_str { ss.str() };
+
+  std::string true_str { "\x1b[44;47mTEST_TEXT" };
 
   EXPECT_EQ(test_str, true_str);
 }
@@ -82,6 +94,42 @@ TEST(TextFormattingTextCustomManipSuite, ColorStyle)
   std::string test_str { ss.str() };
 
   std::string true_str { "\x1b[31;1mTEST_TEXT" };
+
+  EXPECT_EQ(test_str, true_str);
+}
+
+TEST(TextFormattingTextCustomManipSuite, StyleColorBackground)
+{
+  std::stringstream ss;
+
+  ss << tf::setTextParams(tf::TextStyle::BOLD, tf::TextColor::RED, tf::BackgroundColor::CYAN) << "TEST_TEXT";
+  std::string test_str { ss.str() };
+
+  std::string true_str { "\x1b[1;31;46mTEST_TEXT" };
+
+  EXPECT_EQ(test_str, true_str);
+}
+
+TEST(TextFormattingTextCustomManipSuite, BackgroundColorStyle)
+{
+  std::stringstream ss;
+
+  ss << tf::setTextParams(tf::BackgroundColor::MAGENTA, tf::TextColor::RED, tf::TextStyle::BOLD) << "TEST_TEXT";
+  std::string test_str { ss.str() };
+
+  std::string true_str { "\x1b[45;31;1mTEST_TEXT" };
+
+  EXPECT_EQ(test_str, true_str);
+}
+
+TEST(TextFormattingTextCustomManipSuite, ColorBackgroundStyle)
+{
+  std::stringstream ss;
+
+  ss << tf::setTextParams( tf::TextColor::RED, tf::BackgroundColor::YELLOW, tf::TextStyle::BOLD) << "TEST_TEXT";
+  std::string test_str { ss.str() };
+
+  std::string true_str { "\x1b[31;43;1mTEST_TEXT" };
 
   EXPECT_EQ(test_str, true_str);
 }
